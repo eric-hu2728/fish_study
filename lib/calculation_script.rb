38a221d9fbd206a49252d889dae52b06c4480a5e
Jake <jakegreen2016@gmail.com>
2021-02-12 19:03:00 -0900

    have calculation_script.rb run the csv file

--- lib/calculation_script.rb
+++ lib/calculation_script.rb
@@ -1,4 +1,12 @@
+require 'csv'
 puts 'CalcScript initialized.'
 
-contents = File.read('pre.csv')
-puts contents
+averages = Array.new
+
+contents = CSV.open('pre.csv', headers: true, header_converters: :symbol)
+contents.each do |row|
+  x = row[:x_coordinates]
+  y = row[:y_coordinates]
+  
+  puts "x = #{x} and y = #{y}"
+end
